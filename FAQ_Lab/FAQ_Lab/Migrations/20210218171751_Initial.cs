dbeb9d0c6bd37a3a1d796e9fb7bbecdb19c74cff
Calvin Pierce <calvinpierce99@gmail.com>
2021-02-18 12:40:05 -0500

    FAQ Lab Complete

---
+++ FAQ_Lab/FAQ_Lab/Migrations/20210218171751_Initial.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FAQ_Lab.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Topics",
+                columns: table => new
+                {
+                    TopicId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Topics", x => x.TopicId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FAQs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Question = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Answer = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TopicId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    CategoryId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FAQs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FAQs_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_FAQs_Topics_TopicId",
+                        column: x => x.TopicId,
+                        principalTable: "Topics",
+                        principalColumn: "TopicId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "Name" },
+                values: new object[,]
+                {
+                    { "gen", "General" },
+                    { "hist", "History" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Topics",
+                columns: new[] { "TopicId", "Name" },
+                values: new object[,]
+                {
+                    { "cs", "C#" },
+                    { "js", "JavaScript" },
+                    { "boot", "Bootstrap" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "FAQs",
+                columns: new[] { "Id", "Answer", "CategoryId", "Question", "TopicId" },
+                values: new object[,]
+                {
+                    { 1, "A general purpose object-oriented language that uses concise java-like syntax", "gen", "What is C#?", "cs" },
+                    { 5, "In 2002", "hist", "When was C# first released?", "cs" },
+                    { 2, "A general purpose scripting language that executes in a web browser", "gen", "What is JavaScript?", "js" },
+                    { 3, "A CSS framework for creating responsive web apps for multiple screen sizes", "gen", "What is Bootstrap?", "boot" },
+                    { 4, "In 2011", "hist", "When was Boostrap first released?", "boot" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FAQs_CategoryId",
+                table: "FAQs",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FAQs_TopicId",
+                table: "FAQs",
+                column: "TopicId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FAQs");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Topics");
+        }
+    }
+}
---
+++ FAQ_Lab/FAQ_Lab/Models/FAQ.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FAQ_Lab.Models
+{
+    public class FAQ
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public string TopicId { get; set; }
+        public Topic Topic { get; set; }
+        public string CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
